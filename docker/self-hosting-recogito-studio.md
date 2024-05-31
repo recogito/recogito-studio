70391f17a0c27c60b85f632d6218292fd1d4b042
lorinjameson <lwjameson@gmail.com>
2024-05-31 08:35:05 -0400

    WIP

---
+++ docker/self-hosting-recogito-studio.md
@@ -0,0 +1,11 @@
+# Self Hosting Recogito Studio
+
+This guide will help you deploy a self-hosted Recogito Studio installation using docker containers.
+
+## Clone Repo
+
+On the instance that will be hosting Recogito Studio, clone this GitHub repository:
+
+```
+git clone https://github.com/recogito/recogito-studio.git
+```
---
+++ install-self-hosted-docker.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+echo "Starting Recogito Studio Install"
+
+# Load ENV vars
+
+set -o allexport
+source ./docker/.env
+set +o allexport
+
+# Clone the client
+
+echo "Cloning recogito-client"
+
+git clone --depth 1 https://github.com/recogito/recogito-client.git
+
+# Remove the default client config
+rm -f ./recogito-client/src/config.json
+
+# Copy the custom config
+cp ./docker/config/config.json ./recogito-client/src/config.json
+
+# Build the client
+cd ./recogito-client
+
+echo "Building Client docker container"
+
+rm .env
+cp ../docker/.env .env
+
+docker build --no-cache -t recogito-studio-client:latest .
+
+
+# Start docker
+
+echo "Starting Supabase"
+cd ../docker
+
+docker compose pull
+
+docker compose up -d
+
+cd ..
+
+# Clone the server src
+
+echo "Cloning recogito-server"
+
+git clone --depth 1 https://github.com/recogito/recogito-server.git
+
+# Remove the default server config
+rm -f ./recogito-server/config.json
+
+# Copy the custom config
+cp ./docker/config/config.json ./recogito-server/config.json
+
+cd ./recogito-server
+
+npm install
+
+supabase db push --db-url postgresql://postgres:$POSTGRES_PASSWORD@localhost:$POSTGRES_PORT/postgres
+
+sleep 5
+
+node ./create-default-groups.js -f ./config.json
+
